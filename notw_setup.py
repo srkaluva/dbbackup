6ff56707fc65443bb7785100ba0d5d91b2b4fe32
Santosh Kaluva <santoshreddy.kaluva@gmail.com>
2020-03-20 19:59:37 -0400

    wired all modules

--- notw_setup.py
+++ notw_setup.py
@@ -1,24 +1,22 @@
 from setuptools import find_packages, setup
 
 with open('README.md', 'r') as f:
-        long_description = f.read()
+    long_description = f.read()
 
 setup(
     name='dbbackup',
     version='0.1.0',
     author='Santosh Kaluva',
     author_email='skaluva16@gmail.com',
-    description='A utility for backing up PostgreSQL databases',
+    description='utility for backing up db(Postgres)',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/srkaluva/pgbackup',
+    url='https://github.com/srkaluva/dbbackup',
     packages=find_packages('src'),
-    package_dir={'':'src'},
+    package_dir={' ':'src'},
     install_requires=['boto3'],
-    python_requires='>=3.6',
-    entry_points={
-      'console_scripts': [
-        'dbbackup=dbbackup.cli:main',
-      ],
-    }
-)
+    entry_points={'console_scripts': ['dbbackup=dbbackup.cli:main']},
+
+  )
+
+
--- src/dbbackup/cli.py
+++ src/dbbackup/cli.py
@@ -11,11 +11,30 @@ class DriveAction(Action):
 def create_parser():
   parser = ArgumentParser(description="backup postgres DB")
   parser.add_argument("url", help="URL of Database to backup")
-  parser.add_argument("--driver", help="how and where to store backup", nargs=2, action=DriveAction, required=True)
-
-
-
-
+  parser.add_argument("--driver", "-d", help="how and where to store backup", nargs=2, metavar=("DRIVER", "DESTINATION"), action=DriveAction, required=True)
 
   return parser
 
+def main():
+    import time
+    import sys
+    import boto3
+    from dbbackup import pgdump, storage
+
+    args = create_parser().parse_args()
+    dump = pgdump.dump(args.url)
+    if args.driver == 's3' :
+        client = boto3.client('s3')
+        timestamp = time.strftime("%Y-%m-%dT%H:%M", time.localtime())
+        file_name = pgdump.dump_file_name(args.url, timestamp)
+        print(f"Backing database up to {args.destination} in s3 as {file_name}")
+        storage.s3(client, dump.stdout, args.destination, 'example.sql')
+    elif args.driver == 'local':
+        outfile = open(args.destination, 'wb')
+        print(f"Backing database up locally to {outfile.name}")
+        storage.local(dump.stdout, outfile)
+    else:
+        print("invalid args")
+        sys.exit(1)
+
+  
